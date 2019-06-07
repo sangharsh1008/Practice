6ac907879e722452b2e16e2e2f193c1835d5d9a9
sangharsh <spravinkumarsovitkar@cainc.com>
2019-06-07 15:09:47 +0530

    imported temp file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import { Temp } from "./Temp";
+ import { Temp } from "./Temp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
- 
+<Temp/> 
       </header>
     </div>
   );
